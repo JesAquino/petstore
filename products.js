2f538783a5d6edfc55003c3f664449334ea8e6a5
JesAquino <aquinojesuael12@gmail.com>
2023-02-07 10:04:00 +0400

    asd

---
+++ products.js
@@ -0,0 +1,52 @@
+let product = [
+    {
+        "id": 1001,
+        "title": "Cat Food 25lb bag",
+        "description": "A 25 pound bag of <em> irresistible </em>"+"organic goodness for your cat.",
+        "price": 2000,
+        "image":"images/product.jpg",
+        "availableInventory":10,
+        "rating":3
+
+    },
+    {
+        "id": 1002,
+        "title": "Dog Food 25lb bag",
+        "description": "A 25 pound bag of <em> irresistible </em>"+"organic goodness for your dog.",
+        "price": 200,
+        "image":"images/product.jpg",
+        "availableInventory":100,
+        "rating":1
+
+    },
+    {
+        "id": 1003,
+        "title": "Hamster Food 25lb bag",
+        "description": "A 25 pound bag of <em> irresistible </em>"+"organic goodness for your hamster.",
+        "price": 100,
+        "image":"images/product.jpg",
+        "availableInventory":1000,
+        "rating":5
+
+    },
+    {
+        "id": 1004,
+        "title": "Rabbit Food 25lb bag",
+        "description": "A 25 pound bag of <em> irresistible </em>"+"organic goodness for your rabbit.",
+        "price": 1400,
+        "image":"images/product.jpg",
+        "availableInventory":10000,
+        "rating":4
+
+    },{
+        "id": 1005,
+        "title": "Human Food 25lb bag",
+        "description": "A 25 pound bag of <em> irresistible </em>"+"organic goodness for your human.",
+        "price": 912,
+        "image":"images/product.jpg",
+        "availableInventory":100000,
+        "rating":2
+
+    }
+
+]
